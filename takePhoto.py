7bb7e232eab16c9c7ab4d88fd684bf080204ea2c
matt venn <matt@mattvenn.net>
2013-02-03 14:32:51 +0000

    change filename for human friendlyness

--- takePhoto.py
+++ takePhoto.py
@@ -6,9 +6,11 @@ path = "/home/bristolbikeproject/timelapse/images/"
 
 def capture_image():
 	now = datetime.datetime.now()
-	filename = now.strftime("%y-%m-%d-%H-%M-%S")
+	filename = now.strftime("%d-%m-%Y:%H-%M-%S")
 	output = path + filename + '.jpg'
-	command = "fswebcam --set brightness=50%% --no-banner -r 800x600 -d /dev/video0 %s" % output
+    #this can do banners and timestamps
+	command = "fswebcam  --no-banner -r 800x600 -d /dev/video0 %s" % output
+	#command = "fswebcam --set brightness=50%% --no-banner -r 800x600 -d /dev/video0 %s" % output
 	os.system(command)	
 
 
