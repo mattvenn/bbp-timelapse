e929793cb4e8615d537ac68280a93a41cf167266
matt venn <matt@mattvenn.net>
2013-03-07 11:33:11 +0000

    wasn't using the full path for index.html

--- pushPhoto.py
+++ pushPhoto.py
@@ -5,18 +5,30 @@ path = "/home/bristolbikeproject/timelapse/"
 imagepath = path + "images/"
 
 def get_newest():
-	filelist = os.listdir(imagepath)
-	newest = max(filelist, key=lambda x: os.stat(imagepath+x).st_mtime)
-	return newest
-	
+    filelist = os.listdir(imagepath)
+    newest = max(filelist, key=lambda x: os.stat(imagepath+x).st_mtime)
+    return newest
+    
 def scp(fromfile,to):
-	command = "scp %s bbp@mattvenn.net:~/timelapse/%s" % ( fromfile, to )
-	os.system(command)
+    command = "scp %s bbp@mattvenn.net:~/timelapse/%s" % ( fromfile, to )
+    print command
+    os.system(command)
+    print "done"
 
 def mk_index(fromfile):
-	f = open("index.html",'w')
-	f.write('<html><img src="latest.jpg"><p>%s</p></html>' % fromfile)
-	f.close()
+    f = open(path + "index.html",'w')
+    f.write("""
+<html>
+<head>
+<META HTTP-EQUIV="Pragma" CONTENT="no-cache">
+</head>
+<body>
+<h1>%s</h1>
+<img src="latest.jpg">
+<body>
+</html>""" % fromfile)
+    f.close()
+    print "used %s for latest index" % fromfile
 
 if __name__=="__main__":  
     newest = get_newest()
