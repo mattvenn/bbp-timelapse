15d73c18905ed8c2b89185de7d745549340b0ba8
matt venn <matt@mattvenn.net>
2013-03-19 12:48:02 +0000

    added photo resize and youtube embed

--- pushPhoto.py
+++ pushPhoto.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 import os
+import Image
+import re
 
 path = "/home/bristolbikeproject/timelapse/"
 imagepath = path + "images/"
@@ -8,12 +10,38 @@ def get_newest():
     filelist = os.listdir(imagepath)
     newest = max(filelist, key=lambda x: os.stat(imagepath+x).st_mtime)
     return newest
+
+def resize(fromfile,to,width):
+    try:
+        img = Image.open(fromfile)
+        isize = img.size
+        ratio = float(width) / isize[0]
+        newimg = img.resize((int(isize[0]*ratio),int(isize[1]*ratio)),Image.ANTIALIAS)
+        newimg.save(to,"JPEG")
+    except IOError:
+        print "cannot create thumbnail for %s" % fromfile
     
 def scp(fromfile,to):
     command = "scp %s bbp@mattvenn.net:~/timelapse/%s" % ( fromfile, to )
     print command
     os.system(command)
     print "done"
+
+def mk_youtube():
+    f = open(path + "latest_vid")
+    link = f.read()
+    f.close()
+    f = open(path + "youtube.html",'w')
+    m = re.search('watch\?v=([^&]+)&feature',link)
+    embed=m.group(1)
+    print "using %s as embed" % embed
+    f.write("""
+<html>
+<body>
+<iframe width="600" height="450" src="http://www.youtube.com/embed/%s" frameborder="0" allowfullscreen></iframe>
+</body>
+</html>""" % (embed))
+    f.close()
 
 def mk_index(fromfile):
     f = open(path + "latest_vid")
@@ -36,8 +64,18 @@ def mk_index(fromfile):
 
 if __name__=="__main__":  
     newest = get_newest()
-    #the photo
-    scp(imagepath + newest,"latest.jpg")
+
+    #resize for thumbnail
+    small_img_path = path + "latest-296.jpg"
+    resize(imagepath + newest, small_img_path, 296)
+
+    #scp the photo
+    scp(small_img_path,"latest-296.jpg")
+
+    #not making index anymore as going on the bbp website
     #the index
-    mk_index(newest)
-    scp(path + "index.html", "index.html")
+    #mk_index(newest)
+
+    #the youtube embed page
+    mk_youtube()
+    scp(path + "youtube.html", "youtube.html")
