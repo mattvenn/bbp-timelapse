869546ad73f07316d78cebbe4d21c2b3850b7068
matt venn <matt@mattvenn.net>
2013-03-21 15:43:37 +0000

    adds a clock in the top left corner

--- takePhoto.py
+++ takePhoto.py
@@ -3,9 +3,23 @@
 import pickle
 import datetime
 import os
+import PIL
+from PIL import ImageFont
+from PIL import Image
+from PIL import ImageDraw
 path = "/home/bristolbikeproject/timelapse/"
 image_path = path + "images/"
 state = "state"
+
+def add_text_to_image(filename):
+    img = Image.open(filename)
+    now = datetime.datetime.now()
+    text = now.strftime("%H:%M %A")
+    font = ImageFont.truetype("/usr/share/fonts/truetype/freefont/FreeSans.ttf",100)
+    draw = ImageDraw.Draw(img)
+    draw.rectangle([0,0,750,150],fill=(0,0,0))
+    draw.text((10, 10),text,(255,255,255),font=font)
+    img.save(filename,"JPEG")
 
 def capture_image():
     now = datetime.datetime.now()
@@ -23,7 +37,7 @@ def capture_image():
     """ % (shootmode,time,output)
 
     status = os.system(command)  
-    return status
+    return (status,output)
 
 def get_pic_number():
     try:
@@ -39,6 +53,9 @@ def write_pic_number(num):
 
 if __name__=="__main__":  
     num = get_pic_number()
-    if capture_image() == 0:
+    (status,filename) = capture_image()
+    if status == 0:
         print "took photo %d" % num
         write_pic_number(num+1)
+        #add text
+        add_text_to_image(filename)
